7bb893c0a4199f9f5f11609b209655baee32a29c
Alex Hunsley <alex.hunsley@gmail.com>
2025-07-28 12:09:07 +0100

    Add (redundant?) publics

--- Sources/Invariants/WildcardPrototyping.swift
+++ Sources/Invariants/WildcardPrototyping.swift
@@ -3,26 +3,26 @@ public protocol WildcardPrototyping {
 }
 
 public extension WildcardPrototyping {
-    func variants(
+    public func variants(
         _ wildcardPaths: WildcardPath<Self>...
     ) -> [Self] {
         invariantCombinations(self, wildcardPaths: wildcardPaths)
     }
 
-    static func variants(
+    public static func variants(
         _ wildcardPaths: WildcardPath<Self>...
     ) -> [Self] {
         invariantCombinations(Self(), wildcardPaths: wildcardPaths)
     }
 
-    static func variants(
+    public static func variants(
         _ wildcardPaths: [WildcardPath<Self>]
     ) -> [Self] {
         invariantCombinations(Self(), wildcardPaths: wildcardPaths)
     }
 
     // for if you want to pass all variants as one into the test (as a list)
-    static func variantsList(
+    public static func variantsList(
         _ wildcardPaths: WildcardPath<Self>...
     ) -> [[Self]] {
         [invariantCombinations(Self(), wildcardPaths: wildcardPaths)]
