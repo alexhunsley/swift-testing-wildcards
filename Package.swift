f0598e817360a0fe45827628b7ab33f5e21b6e17
Alex Hunsley <alex.hunsley@gmail.com>
2025-07-28 10:22:13 +0100

    Add manually created Package.swift at top level for SPM publishing

---
+++ Package.swift
@@ -0,0 +1,38 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+
+// AH: this was created manually for SPM publishing
+let package = Package(
+    name: "TestingWildcards",
+    platforms: [
+        .iOS(.v17)
+    ],
+    products: [
+        .library(
+            name: "TestingWildcards",
+            targets: ["TestingWildcards"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "TestingWildcards",
+            path: "Sources",
+            sources: [
+                "TestingWildcards.swift",
+                "TestingWildcardsApp.swift",
+                "Helpers",
+                "Invariants"
+            ]
+        ),
+        .testTarget(
+            name: "TestingWildcardsTests",
+            dependencies: ["TestingWildcards"],
+            path: "Tests",
+            sources: [
+                "TestingWildcardsTests.swift",
+                "WildcardTestHelpers.swift"
+            ]
+        )
+    ]
+)
