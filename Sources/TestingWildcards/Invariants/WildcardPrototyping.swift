343af26e17763e1a8c99eab260fd1892f3cc0a5e
Alex Hunsley <alex.hunsley@gmail.com>
2025-08-04 10:48:17 +0100

    Remove as-array versions of .variants and .variantsList (the variadic is fine)

--- Sources/TestingWildcards/Invariants/WildcardPrototyping.swift
+++ Sources/TestingWildcards/Invariants/WildcardPrototyping.swift
@@ -15,14 +15,14 @@ public extension WildcardPrototyping {
         invariantCombinations(Self(), wildcardPaths: wildcardPaths)
     }
 
-    static func variants(
-        _ wildcardPaths: [WildcardPath<Self>]
-    ) -> [Self] {
-        invariantCombinations(Self(), wildcardPaths: wildcardPaths)
-    }
-
     /// for retrieving all variants as a (single item) list of lists; particularly useful
     /// if you want to pass a single list of variants into a @Test func.
+    func variantsList(
+        _ wildcardPaths: WildcardPath<Self>...
+    ) -> [[Self]] {
+        [invariantCombinations(self, wildcardPaths: wildcardPaths)]
+    }
+
     static func variantsList(
         _ wildcardPaths: WildcardPath<Self>...
     ) -> [[Self]] {
