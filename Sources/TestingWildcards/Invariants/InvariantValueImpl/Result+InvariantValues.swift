60c6533ebfd1d7eb15eff48ed298b2f226c1f069
Alex Hunsley <alex.hunsley@gmail.com>
2025-08-04 10:47:39 +0100

    Result stuff: add InvariantValues for Never, and my own Void-alike `Empty`

--- Sources/TestingWildcards/Invariants/InvariantValueImpl/Result+InvariantValues.swift
+++ Sources/TestingWildcards/Invariants/InvariantValueImpl/Result+InvariantValues.swift
@@ -9,3 +9,34 @@ extension Result: InvariantValues where Success: InvariantValues & Equatable, Fa
         return AnySequence(successVariants + failureVariants)
     }
 }
+
+/// the allValues for Never is just an empty list -- i.e. the  identiity, because algebraicly it multiples combinations by 1
+extension Never: InvariantValues {
+    public static var allValues: AnySequence<Never> {
+        AnySequence(EmptyCollection<Never>())
+    }
+}
+
+// can't! Void is non-nominal.
+//extension Void: InvariantValues {
+//
+//}
+
+// distinction:
+//
+// Void is non-nominal tuple '()' for 'no return type' (but this result CAN return, just nothing).
+// Never means it can't return, because `enum Never { }` is not instantiable.
+//
+
+
+/// A nominal stand-in for Void (no value)
+public struct Empty: Equatable, Sendable { }
+public let empty = Empty()
+
+extension Empty: InvariantValues {
+    public static var allValues: AnySequence<Empty> {
+        // there’s exactly one inhabitant of Unit,
+        // so we return a sequence containing a single .init()
+        return AnySequence([empty])
+    }
+}
